85e6ad98a96bb230a38f9be21437e90acf48fd32
Patryk Futa <patryk.futa@gmail.com>
2021-12-24 19:28:27 +0100

    Remove NAT

--- base-infrastructure/vpc.tf
+++ base-infrastructure/vpc.tf
@@ -103,3 +103,29 @@ resource "aws_route_table_association" "main-public-3-a" {
   subnet_id      = aws_subnet.main-public-3.id
   route_table_id = aws_route_table.main-public.id
 }
+
+resource "aws_route_table" "main-private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw.id
+  }
+
+  tags = {
+    ProjectName = var.project_name
+  }
+}
+
+resource "aws_route_table_association" "main-private-1-a" {
+  subnet_id      = aws_subnet.main-private-1.id
+  route_table_id = aws_route_table.main-private.id
+}
+resource "aws_route_table_association" "main-private-2-a" {
+  subnet_id      = aws_subnet.main-private-2.id
+  route_table_id = aws_route_table.main-private.id
+}
+resource "aws_route_table_association" "main-private-3-a" {
+  subnet_id      = aws_subnet.main-private-3.id
+  route_table_id = aws_route_table.main-private.id
+}
