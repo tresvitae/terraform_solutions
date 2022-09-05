d76abd710804265ede346d627239686d11e5c07d
tresvitae <patryk.futa@gmail.com>
2022-09-05 20:21:06 +0200

    Update MariaDB version to 10.6

--- rds/rds.tf
+++ rds/rds.tf
@@ -1,31 +1,31 @@
-resource "aws_db_subnet_group" "mariadb-subnet" {
-  name = "mariadb-subnet"
+resource "aws_db_subnet_group" "mariadb_subnet" {
+  name        = "mariadb-subnet"
   description = "RDS subnet group"
-  subnet_ids  = [aws_subnet.main-private-1.id, aws_subnet.main-private-2.id]
+  subnet_ids  = [aws_subnet.main_private_1.id, aws_subnet.main_private_2.id]
 }
 
-resource "aws_db_parameter_group" "mariadb-parameters" {
+resource "aws_db_parameter_group" "mariadb_parameters" {
   name        = "mariadb-parameters"
-  family      = "mariadb10.4"
+  family      = "mariadb10.6"
   description = "MariaDB parameter group"
 }
 
 resource "aws_db_instance" "mariadb" {
   allocated_storage       = 100
   engine                  = "mariadb"
-  engine_version          = "10.4.13"
+  engine_version          = "10.6.8"
   instance_class          = var.db_instance_type
   identifier              = "mariadb"
-  name                    = "mariadb"
+  db_name                 = "mariadb"
   username                = var.RDS_USERNAME
   password                = var.RDS_PASSWORD
-  db_subnet_group_name    = aws_db_subnet_group.mariadb-subnet.name
-  parameter_group_name    = aws_db_parameter_group.mariadb-parameters.name
+  db_subnet_group_name    = aws_db_subnet_group.mariadb_subnet.name
+  parameter_group_name    = aws_db_parameter_group.mariadb_parameters.name
   multi_az                = false
-  vpc_security_group_ids  = [aws_security_group.allow-mariadb.id]
+  vpc_security_group_ids  = [aws_security_group.allow_mariadb.id]
   storage_type            = "gp2"
   backup_retention_period = 30
-  availability_zone       = aws_subnet.main-private-1.availability_zone
+  availability_zone       = aws_subnet.main_private_1.availability_zone
   skip_final_snapshot     = true
 
   tags = {
