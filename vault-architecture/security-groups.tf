a8ddf7c0aceec31f66df6afc75201cf40f23c82f
Patryk <patryk.futa@gmail.com>
2023-11-07 19:49:41 +0100

    Add AWS VPC network

---
+++ vault-architecture/security-groups.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "ssh" {
+    name = "${var.environment_name}-ssh-sg"
+    descritpion = "SSH and Internal Traffic"
+    vpc_id = module.vault_vpc.vpc_id
+
+    tags = {
+        Name = var.environment_name
+    }
+
+    # SSH
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Vault API traffic
+    ingress {
+        from_port = 8200
+        to_port = 8200
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Internal traffic
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        self = true
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
