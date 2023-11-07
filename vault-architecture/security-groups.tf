aad9c011db7de37e99d5730069171c446bf83ea3
Patryk <patryk.futa@gmail.com>
2023-11-07 19:51:32 +0100

    Reformat

--- vault-architecture/security-groups.tf
+++ vault-architecture/security-groups.tf
@@ -1,40 +1,40 @@
 resource "aws_security_group" "ssh" {
-    name = "${var.environment_name}-ssh-sg"
-    descritpion = "SSH and Internal Traffic"
-    vpc_id = module.vault_vpc.vpc_id
+  name        = "${var.environment_name}-ssh-sg"
+  description = "SSH and Internal Traffic"
+  vpc_id      = module.vault_vpc.vpc_id
 
-    tags = {
-        Name = var.environment_name
-    }
+  tags = {
+    Name = var.environment_name
+  }
 
-    # SSH
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    # Vault API traffic
-    ingress {
-        from_port = 8200
-        to_port = 8200
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  # Vault API traffic
+  ingress {
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    # Internal traffic
-    ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        self = true
-    }
+  # Internal traffic
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
